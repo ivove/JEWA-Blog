fcafb68ea23b550daae6895c84c5381b83a1f7f1
IVo Ver Eecke <ivo_ver_eecke@hotmail.com>
2022-01-29 17:30:49 +0100

    the basics

---
+++ src/JEWA-Blog/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JEWA_Blog.Models
+{
+    public class Comment
+    {
+        [Required]
+        public string CommentId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public DateTime PublicationTime { get; set; } = DateTime.UtcNow;
+
+        public string Author { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        public string PostId { get; set; } = string.Empty;
+
+        public string ReplyTo { get; set; } = string.Empty;
+
+        public List<Comment> Replies { get; set; } = new List<Comment>();
+    }
+}
---
+++ src/JEWA-Blog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JEWA_Blog.Models
+{
+    public class Post
+    {
+        [Required]
+        public string PostId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        public string Excerpt { get; set; } = string.Empty;
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        public bool IsPublished { get; set; } = false;
+
+        public DateTime? PublicationDate { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}

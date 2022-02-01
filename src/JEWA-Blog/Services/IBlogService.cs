1b33c007f2d1ed597b799ea272b263bb9b18b1b3
IVo Ver Eecke <ivo_ver_eecke@hotmail.com>
2022-02-01 20:45:52 +0100

    data access and some sample XML-files

---
+++ src/JEWA-Blog/Services/IBlogService.cs
@@ -0,0 +1,17 @@
+﻿using JEWA_Blog.Models;
+
+namespace JEWA_Blog.Services
+{
+    public interface IBlogService
+    {
+        Task SavePost(Post post);
+        Task DeletePost(Post post);
+
+        Task<List<string>> GetCategories();
+
+        Task<Post> GetPostById(string id);
+        Task<Post> GetPostByTitle(string title);
+
+        Task<List<Post>> GetPosts(int count,int skip=0,string category="");
+    }
+}
